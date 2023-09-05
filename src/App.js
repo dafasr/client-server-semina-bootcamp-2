156ebbe121858707e13cac4617bd282679778f80
dafasr <dafashanrizki2200@gmail.com>
2023-09-05 11:24:17 +0700

    -

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  const Hello = () => 'Hello';
-  return (
-    <div className="App">
-      <h1>{Hello()}</h1>
-    </div>
-  );
+  // const Hello = () => "Hello";
+  // function Hello() {
+  //   'Hello'
+  // }
+  // const Hello =function(){
+  //   'Hello'
+  // }
+  return <h1>Welcome to React</h1>;
 }
 
 export default App;
