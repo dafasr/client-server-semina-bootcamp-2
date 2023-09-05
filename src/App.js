ba001534955f2f7b1f6c0fdb43d53422ebb3bcf2
dafasr <dafashanrizki2200@gmail.com>
2023-09-05 21:36:05 +0700

    -

--- src/App.js
+++ src/App.js
@@ -1,74 +1,36 @@
-// import logo from "./logo.svg";
-
 import "./App.css";
-import { Hello } from "./Hello";
-import { Title } from "./Title";
 import Button from "./components/Button";
-import Table from "./components/Table";
-
+import { useState } from "react";
 function App() {
-  // const Hello = () => "Hello";
-  // function Hello() {
-  //   'Hello'
-  // }
-  // const Hello =function(){
-  //   'Hello'
-  // }
+  const [number, setNumber] = useState(0);
+  const [name, setName] = useState("");
+  const [tahunLahir, setTahunLahir] = useState("");
 
-  // const users = ["yohannes", "ongki", "abu"];
-  const users = [
-    {
-      _id: 1,
-      name: "Dafa Shanrizki",
-      age: 21,
-      status: true,
-    },
-    {
-      _id: 2,
-      name: "Roy",
-      age: 23,
-      status: true,
-    },
-    {
-      _id: 3,
-      name: "Aldy",
-      age: 31,
-      status: false,
-    },
-  ];
+  console.log(number);
 
-  const isLogin = false;
+  const klik = () => {
+    setNumber(number + 1);
+  };
+  const handleSubmit = () => {};
 
   return (
     <>
-      <h1>Welcome to React</h1>
-      <ul>
-        <li>Home</li>
-        <li>Users</li>
-        <li>{isLogin ? "Sudah Login" : "Login"}</li>
-      </ul>
-      <h1>
-        <Hello />
-      </h1>
-      <Title name="Dafa Shanrizki" />
-      {/* <br />
-        <Title /> */}
+      <h1>Counter app</h1>
+      <p>Nilai counter saat ini {number} kali</p>
+      <Button onClick={klik}>Click me</Button>
+      <hr />
+      <h1>Aplikasi input data diri</h1>
+      Name :
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
       <br />
-      <Button onClick={() => alert("click save")}>Save</Button>
-      <Table users={users} />
-      {/* <ul>
-        {users.map((user, index) => {
-          return (
-            <>
-              {user.status && (
-                <li
-                  key={index}
-                >{`Nama Saya ${user.name} dan usia saya ${user.age} tahun`}</li>
-              )}
-            </>
-          );
-        })}
-      </ul> */}
+      <br />
+      Tahun Lahir :
+      <input type="text" value={tahunLahir} />
+      <Button onClick={handleSubmit}>Submit</Button>
     </>
   );
 }
